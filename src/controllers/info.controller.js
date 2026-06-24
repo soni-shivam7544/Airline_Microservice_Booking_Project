fcb4e1e277bed624b22919be4e3f3ae1aa27cbbf
soni-shivam7544 <sonishivam7544@gmail.com>
2026-06-24 19:29:26 +0530

    Added basic express functionalities.

---
+++ src/controllers/info.controller.js
@@ -0,0 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
+
+const info = (req, res) => {
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        msg: "API is live."
+    });
+}
+
+module.exports = {
+    info
+}
